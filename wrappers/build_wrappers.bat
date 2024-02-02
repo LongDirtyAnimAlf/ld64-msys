e098909b36bd35ec0b876ee7e46c40b956ab3871
Don <LongDirtyAnimAlf@gmail.com>
2024-02-02 08:32:47 +0000

    Add wrapper for dsymutil and strip on Windows.

--- wrappers/build_wrappers.bat
+++ wrappers/build_wrappers.bat
@@ -29,11 +29,20 @@ call %FPC% %SYSTEM% -di386_darwin -dLD -o.\fpc-wrappers\i386-apple-darwin19-%TAR
 call %FPC% %SYSTEM% -darm64_ios -dLD -o.\fpc-wrappers\aarch64-apple-ios14-%TARGET% -FU.\fpc-wrappers\lib wrapper.lpr
 call %FPC% %SYSTEM% -darm_ios -dLD -o.\fpc-wrappers\arm-apple-ios10-%TARGET% -FU.\fpc-wrappers\lib wrapper.lpr
 
-echo CLANG
-echo DSYMUTIL
-echo LD
-echo STRIP
+set TARGET=dsymutil.exe
 
+call %FPC% %SYSTEM% -damd64_darwin -dDSYMUTIL -o.\fpc-wrappers\x86_64-apple-darwin19-%TARGET% -FU.\fpc-wrappers\lib wrapper.lpr
+call %FPC% %SYSTEM% -darm64_darwin -dDSYMUTIL -o.\fpc-wrappers\aarch64-apple-darwin19-%TARGET% -FU.\fpc-wrappers\lib wrapper.lpr
+call %FPC% %SYSTEM% -di386_darwin -dDSYMUTIL -o.\fpc-wrappers\i386-apple-darwin19-%TARGET% -FU.\fpc-wrappers\lib wrapper.lpr
+call %FPC% %SYSTEM% -darm64_ios -dDSYMUTIL -o.\fpc-wrappers\aarch64-apple-ios14-%TARGET% -FU.\fpc-wrappers\lib wrapper.lpr
+call %FPC% %SYSTEM% -darm_ios -dDSYMUTIL -o.\fpc-wrappers\arm-apple-ios10-%TARGET% -FU.\fpc-wrappers\lib wrapper.lpr
 
+set TARGET=strip.exe
+
+call %FPC% %SYSTEM% -damd64_darwin -dSTRIP -o.\fpc-wrappers\x86_64-apple-darwin19-%TARGET% -FU.\fpc-wrappers\lib wrapper.lpr
+call %FPC% %SYSTEM% -darm64_darwin -dSTRIP -o.\fpc-wrappers\aarch64-apple-darwin19-%TARGET% -FU.\fpc-wrappers\lib wrapper.lpr
+call %FPC% %SYSTEM% -di386_darwin -dSTRIP -o.\fpc-wrappers\i386-apple-darwin19-%TARGET% -FU.\fpc-wrappers\lib wrapper.lpr
+call %FPC% %SYSTEM% -darm64_ios -dSTRIP -o.\fpc-wrappers\aarch64-apple-ios14-%TARGET% -FU.\fpc-wrappers\lib wrapper.lpr
+call %FPC% %SYSTEM% -darm_ios -dSTRIP -o.\fpc-wrappers\arm-apple-ios10-%TARGET% -FU.\fpc-wrappers\lib wrapper.lpr
 
 echo "Done !"
